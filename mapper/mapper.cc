e700cd7ba8a20a7bfd42c1f83c483e6f3f156167
Grant Kluber <gkluber@cs.utexas.edu>
2019-04-28 14:39:50 -0500

    Started mapper

--- mapper/mapper.cc
+++ mapper/mapper.cc
@@ -1,1 +1,71 @@
 #include "core.h"
+
+#include <iostream>
+
+bool read_ines(const char *name)
+{
+	ifstream rom;
+	rom.open(name, ios::in | ios::binary);
+	if(!rom.is_open())
+	{
+		std::cerr << "Could not open rom " << name << "!";
+		return false;
+	}
+	
+	char header[16];
+	rom.read(header, 16);	
+	if(header[0] != 'N' || header[1] != 'E' || header[2] != 'S' || header[3] != 0x1a)
+	{
+		std::cerr << "Rom " << name << " has invalid header file!";
+		return false;
+	}
+	
+	char prg_size = header[4];
+	char chr_size = header[5]; // TODO check what it means if this equals zero
+	char f6 = header[6];
+	char f7 = header[7];
+	char f8 = header[8];
+	char f9 = header[9];
+
+	// Decode flags 6
+	bool mirroring = f6 & 0b1;	
+	bool containsBattery = (f6 & 0b10) >> 1;
+	bool hasTrainer = (f6 & 0b100) >> 2;
+	bool ignoreMirroring = (f6 & 0b1000) >> 3;
+	uint8_t loMapper = (f6 & 0xf0) >> 4;
+	
+	// Decode flags 7
+	bool vs = f7 & 0b1; // VS games
+	bool playchoice = (f7 & 0b10) >> 1;
+	bool nes2 = (f7 & 0b1100) == 0b1000;
+	uint8_t hiMapper = (f7 & 0xf0);
+	
+	// Decode flags 8
+	// only used if containsBattery == true	
+	uint8_t prg_ram_size = f8;
+	
+	// Decode flags 9
+	bool pal = f9 & 0b1; // if true, use PAL TV system. Otherwise use NTSC.	
+
+	// Implementing the rule of thumb stated here under section Flag 10
+	// http://wiki.nesdev.com/w/index.php/INES	
+	bool lastZero = header[12] == 0 && header[13] = 0 && header[14] == 0 && header[15] == 0;
+
+	// TODO parse NES 2.0 and iNES differently
+	uint8_t mapper;
+	// See if we need to make out the upper four bits of the mapper	
+	if(!lastZero && !nes2)
+	{
+		mapper = loMapper;
+	}
+	else
+	{
+		mapper = hiMapper | loMapper;	
+	}
+	
+	
+ 
+	
+	
+	rom.close();
+}
