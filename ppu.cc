78f318f75239671ea0c0343d5fa136cb12762e9b
gkluber <gkluber@cs.utexas.edu>
2019-04-30 21:31:04 -0500

    PPU outline

--- ppu.cc
+++ ppu.cc
@@ -7,16 +7,47 @@ using namespace PPU;
 
 namespace PPU
 {
+	// PPU internal registers
+	uint16_t v; // Current VRAM address
+	uint16_t t; // Temporary VRAM address
+	uint16_t x; // Fine X scroll
+	uint16_t w; // First or second write toggle
+
+	// Exposed registers	
+	uint8_t ppuctrl;
+	uint8_t ppumask;
+	uint8_t ppustatus;
+	uint8_t oamaddr;
+	uint8_t oamdata;
+	// TODO scroll and ppuaddr latches
+	uint8_t ppuscroll;
+	uint8_t ppuaddr;
+	uint8_t ppudata;
+	bool odd_frame;
+	
 	uint8_t mem[16384];
 	
-	//TODO
+	
+		
 	void power()
 	{
-		// TODO initialize things
+		ppuctrl = 0;
+		ppumask = 0;
+		ppustatus = 0b10100000;
+		oamaddr = 0;
+		ppuscroll = 0;
+		ppuaddr = 0;
+		ppudata = 0;
+		odd_frame = false;
 	}
 	
 	void reset()
 	{
-		// TODO change things
+		ppuctrl = 0;
+		ppumask = 0;
+		ppustatus &= 0b10000000;
+		ppuscroll = 0;
+		ppudata = 0;
+		odd_frame = false;
 	}
 }
--- ppu.h
+++ ppu.h
@@ -3,9 +3,20 @@
 #include <cstdint>
 
 namespace PPU
-{
+{	
+	extern uint8_t ppuctrl;
+	extern uint8_t ppumask;
+	extern uint8_t ppustatus;
+	extern uint8_t oamaddr;
+	extern uint8_t oamdata;
+	// TODO scroll and ppuaddr latches
+	extern uint8_t ppuscroll;
+	extern uint8_t ppuaddr;
+	extern uint8_t ppudata;
+	extern bool odd_frame;
+	
 	extern uint8_t mem[16384];
-
+	
 	void power();
 	void reset();
 }
